f5c691f2eb0e754dd97d0ab7a148f1bc8bdf8c9f
Safia TELDJI <feufoleil@MSI.localdomain>
2022-09-15 16:37:04 +0200

    fixed an aesthetic issue with the dlgbrd

--- jsfiles/homedlglines.js
+++ jsfiles/homedlglines.js
@@ -1,6 +1,6 @@
 var dlgs = { 
     "dlg1" : "Hello ! Welcome here. Please, click on the button on the bottom right to start reading what I have to say~",
-    "dlg2" : "I'm Safia and I made this tiny program to show you my digital resume. I hope you'll enjoy it !",
+    "dlg2" : "I'm Safia and I made this interactive page to show you my digital resume. I hope you'll enjoy it !",
     "dlg3" : 'Please, feel free to click on the menu on your left and choose an option to know more about me~',
     "dlg4" : "Thank you so much ! And see you soon !" ,
  }; 
