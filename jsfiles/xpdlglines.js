cfad5d9adf7cfc24fc439379604753dd172f8e2d
Safia TELDJI <feufoleil@MSI.localdomain>
2022-09-16 15:01:41 +0200

    Made it responsive on phones (320x480)

--- jsfiles/xpdlglines.js
+++ jsfiles/xpdlglines.js
@@ -1,7 +1,7 @@
 var dlgs = { 
     "dlg1" : "Now let's talk about my experiences !",
     "dlg2" : "What did I do ? Well, I touched a lot of areas in my life.",
-    "dlg3" : "Here, you can read in this new window a few informations about my education level !" ,
+    "dlg3" : "Here, you can read it in this new window about my education level !" ,
     "dlg4" : "I also did work in a few places before, I know how to deal with working life.",
     "dlg5" : "Please take a look at this new window to know more about it~",
  }; 
