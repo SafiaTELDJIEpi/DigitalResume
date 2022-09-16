1dcea534a87e727164abfa6c548c92b3e7261218
Safia TELDJI <feufoleil@MSI.localdomain>
2022-09-16 10:28:07 +0200

    added more dlg lines and built other pages

--- jsfiles/skillsdlg.js
+++ jsfiles/skillsdlg.js
@@ -5,12 +5,24 @@ document.getElementById("dlgtext").innerHTML = dlgs[currentdlg] ;
 document.getElementById("dlgbutton").onclick = function() {
         if(currentdlg == "dlg1" ) {
                 currentdlg = "dlg2" ;
+                document.getElementById('windowprogskills').style.display = "block";
         }
         else if (currentdlg == "dlg2") {
                        currentdlg = "dlg3" ;
                 } 
         else if (currentdlg == "dlg3") {
                         currentdlg = "dlg4";
+        }
+        else if (currentdlg == "dlg4") {
+                        currentdlg = "dlg5";
+                        document.getElementById('windowprogskills').style.display = "none"
+        }
+        else if (currentdlg == "dlg5") { 
+                        currentdlg = "dlg6";
+        }
+        else if (currentdlg == "dlg6") {
+                        currentdlg = "dlg7";
+                        document.getElementById('windowotherskills').style.display = "block";
         }
         document.getElementById("dlgtext").innerHTML = dlgs[currentdlg] ;
 }  
--- jsfiles/skillsdlglines.js
+++ jsfiles/skillsdlglines.js
@@ -1,6 +1,9 @@
 var dlgs = { 
-    "dlg1" : "skills?",
-    "dlg2" : 'ça se mange ?',
-    "dlg3" : "ouuuukayyyyy" ,
-    "dlg4" : "on est là hein",
+    "dlg1" : "I did work on a few programming languages so far",
+    "dlg2" : "Here, take a look !",
+    "dlg3" : "I know how to code in these programming languages." ,
+    "dlg4" : "I'm also learning how to swim in PHP, JQuery and took an interest about (en choisir un car il y en a trop qui m'interessent et je suis AHHHH ?!?!) ",
+    "dlg5" : "I also did learn a few other skills",
+    "dlg6" : "Such as organizing my technologies, working with a team...",
+    "dlg7" : "You can read more about them here~",
  }; 
