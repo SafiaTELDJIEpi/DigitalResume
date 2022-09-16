9c487f60d38459bdc0302d5576db91affb718892
Safia TELDJI <feufoleil@MSI.localdomain>
2022-09-16 22:02:09 +0200

    Finished all of the pages and made it responsive

--- jsfiles/skillsdlglines.js
+++ jsfiles/skillsdlglines.js
@@ -2,7 +2,7 @@ var dlgs = {
     "dlg1" : "I did work with a few programming languages so far",
     "dlg2" : "Here, take a look !",
     "dlg3" : "I know how to code in these programming languages." ,
-    "dlg4" : "I'm also learning how to swim in PHP, JQuery and took an interest about (en choisir un car il y en a trop qui m'interessent et je suis AHHHH ?!?!) ",
+    "dlg4" : "I'm also learning how to swim in PHP and JQuery. ",
     "dlg5" : "I also did learn a few other skills",
     "dlg6" : "Such as organizing my technologies, working with a team...",
     "dlg7" : "You can read more about them here~",
